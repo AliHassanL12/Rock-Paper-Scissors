7d34b133a6b21c14d35b4c6c4cf9570d27479ec5
Ali Hassan Liaquat <liaqqahl@gmail.com>
2023-01-24 01:57:14 +0000

    Initialised project

---
+++ script.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    let computerChoice = ['rock', 'paper', 'scissors'];
+    var randomComputerChoice = Math.floor(Math.random()*computerChoice.lenght);
+}
+
+console.log(getComputerChoice())
